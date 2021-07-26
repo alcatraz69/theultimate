9e0ea0460cfdf92443f53acaf6ab12bf8391b894
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-07-26 20:53:44 +0530

    updated blog links and images

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Blogs.module.css";
 import blog1 from "./BlogAssests/blog1.png";
+import blog2 from "./BlogAssests/asyncawaitjs.jpg";
+import blog3 from "./BlogAssests/opc.jpg";
 
 const Blogs = () => {
   return (
@@ -10,39 +12,37 @@ const Blogs = () => {
       </h1>
       <div className={styles.container}>
         <div className={styles.square}>
-          <img
-            src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80"
-            className={styles.blogImg}
-            alt="blog1 pic"
-          ></img>
-          <div className={styles.h1}>Is Apple a Design Company?</div>
-          <p className={styles.blogDesc}>
-            Apple is more than a tech company; it became a culture unto itself,
-            a passion of most of people and the birthplace of the world’s most
-            revolutionized products.
-          </p>
-
-          <div>
-            <a
-              href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261"
-              target="_"
-              className={styles.button}
-            >
-              Read More
-            </a>
-          </div>
-        </div>
-        <div className={styles.square}>
           <img src={blog1} className={styles.blogImg} alt="blog2 pic"></img>
-          <div className={styles.h1}>Wonderful Web Communication!</div>
+          <div className={styles.h1}>
+            Wonderful Web Communication! Connecting to the Internet
+          </div>
           <p className={styles.blogDesc}>
             In this blog I have explained how the communication bewteen
             computers and other devices over the internet takes place.
           </p>
 
-          <div>
+          <div className={styles.btnSec}>
             <a
-              href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261"
+              href="https://hashnode.com/post/wonderful-web-communication-ckqmdafwu04pbs2s1ctucb66r"
+              target="_"
+              className={styles.button}
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+        <div className={styles.square}>
+          <img src={blog2} className={styles.blogImg} alt="blog1 pic"></img>
+          <div className={styles.h1}>Asynchronous Javascript - Async/Await</div>
+          <p className={styles.blogDesc}>
+            Async/Await pattern is a syntactic feature of Javascript language
+            that allows an asynchronous, blocking function to be structured in a
+            way similar to an ordinary synchronous function.
+          </p>
+
+          <div className={styles.btnSec}>
+            <a
+              href="https://hashnode.com/post/asynchronous-javascript-asyncawait-ckr2e58oq0mwvpts1cr4r7jqx"
               target="_"
               className={styles.button}
             >
@@ -53,20 +53,21 @@ const Blogs = () => {
 
         <div className={styles.square}>
           <img
-            src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80"
+            // src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80"
+            src={blog3}
             className={styles.blogImg}
             alt="blog3 pic"
           ></img>
-          <div className={styles.h1}>Is Apple a Design Company?</div>
+          <div className={styles.h1}>Optional Chaining - Javascript ES2020</div>
           <p className={styles.blogDesc}>
-            Apple is more than a tech company; it became a culture unto itself,
-            a passion of most of people and the birthplace of the world’s most
-            revolutionized products.
+            The optional chaining operator provides a way to simplify accessing
+            values through connected objects when it's possible that a reference
+            or function may be undefined or null .
           </p>
 
-          <div>
+          <div className={styles.btnSec}>
             <a
-              href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261"
+              href="https://hashnode.com/post/optional-chaining-javascript-es2020-ckr549zk4033rrfs10olr85h0"
               target="_"
               className={styles.button}
             >
